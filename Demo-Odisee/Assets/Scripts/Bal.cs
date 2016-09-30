eab7bcfb863f0cfb2a77bc341884b835e747bc60
Jonathan Van Beneden <jonavb123@gmail.com>
2016-09-30 20:49:30 +0200

    Initial Commit
    Powerpoint presentation,
    Notes for demo,
    Live Odisee Brussels Demo Application

---
+++ Demo-Odisee/Assets/Scripts/Bal.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bal : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name.Equals("OVRPlayerController"))
+        {
+            Captured();
+        }
+    }
+
+    void Captured()
+    {
+        var world = GameObject.FindGameObjectWithTag("World");
+        if (world != null)
+        {
+            world.BroadcastMessage("AddItem");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Demo-Odisee/Assets/Scripts/Ui.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ui : MonoBehaviour {
+
+    private int _counter;
+
+    public float X, Y;
+    public float Width, Height;
+
+    private Vector2 _pos;
+    private Vector2 _size;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _pos = new Vector2(X, Y);
+        _size = new Vector2(Width, Height);
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(_pos, _size), _counter.ToString());
+    }
+
+    void AddItem()
+    {
+        _counter++;
+    }
+}
